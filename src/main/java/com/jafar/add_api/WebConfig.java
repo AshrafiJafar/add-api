81886ff08d828e485b9471f712a6891153044543
AshrafiJafar <ashrafij007@gmail.com>
2025-04-30 14:32:08 +0300

    allowed origin updated

--- src/main/java/com/jafar/add_api/WebConfig.java
+++ src/main/java/com/jafar/add_api/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*") // Allow all origins
+                .allowedOriginPatterns("http://localhost:*", "http://*.amazonaws.com") // Allow all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true) // Keep this if you're handling credentials
